2ef12bb21bdff7dec2df3c5b11a8a7719a2c6ced
jaimil3648-cmd <jaimil.3648@gmail.com>
2026-06-24 11:30:50 +0930

    Main menu

---
+++ RoadIQ/Assets/Jaimil/Script/Speed_O_Meter.cs
@@ -0,0 +1,34 @@
+using UnityEngine;
+using TMPro;
+
+public class Speedometer : MonoBehaviour
+{
+    [Header("References")]
+    public Rigidbody carRb;
+    public TextMeshProUGUI speedText;
+
+    [Header("Settings")]
+    public bool showKPH = true;
+
+    private float currentSpeed;
+
+    void Update()
+    {
+        if (carRb == null || speedText == null)
+            return;
+
+        // Calculate speed
+        currentSpeed = carRb.velocity.magnitude;
+
+        if (showKPH)
+        {
+            float speedKPH = currentSpeed * 3.6f;
+            speedText.text = Mathf.RoundToInt(speedKPH) + " km/h";
+        }
+        else
+        {
+            float speedMPH = currentSpeed * 2.237f;
+            speedText.text = Mathf.RoundToInt(speedMPH) + " mph";
+        }
+    }
+}
